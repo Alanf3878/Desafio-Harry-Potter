46a3445ca88443e47300cfbeccaee11b0b8a05a7
Alan Fernandes <81441721+Alanf3878@users.noreply.github.com>
2021-09-27 11:47:35 -0300

    Update index.js

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,9 +2,8 @@ import React from 'react'
 import Data from './Date'
 import styled from 'styled-components'; 
 import Img from "../../images/rock.jpg"
-const Divs = styled.div`
-
- 
+const Divs = styled.div
+`
   display: flex;
   flex-wrap: wrap;
   width: 80%;
@@ -15,9 +14,6 @@ const Divs = styled.div`
   margin-top: 20px;
   border-radius: 20px;
   border: solid #1D334A 2px;
-  
-
-
 `
 
 export default function Main({character}){
