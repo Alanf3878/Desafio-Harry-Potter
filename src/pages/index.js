b18e628052e1a84e0a48aeb006c3a5b00dc42595
Alan Fernandes <81441721+Alanf3878@users.noreply.github.com>
2021-09-29 09:31:46 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,16 @@ import Main from "../components/Main"
 import {Header} from "../components/Header"
 import {Footer} from "../components/footer"
 import "./index.css"
-
-
 export default function Index() {
   const [character, setcharacter] = useState([])   
-
   useEffect( () => {
    handleGetUserData()
   }, [])
-
   async function handleGetUserData(){
     const response = await fetch("http://hp-api.herokuapp.com/api/characters")
     const data = await response.json()
     setcharacter(data)
   }
-  
   return( 
     <div className = "Background">
       <Header/>
